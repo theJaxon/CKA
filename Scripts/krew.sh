b1c880e5ea02d274ad526f43e177f48b3a78958f
Jaxon <mohamedmostels@gmail.com>
2020-09-01 16:43:01 +0200

    Create krew.sh

---
+++ Scripts/krew.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Script made to install kubectl krew plugin on linux distros
+
+echo "[TASK 1] Downloading krew from Github"
+curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/krew.tar.gz"
+
+echo "[TASK 2] extracting krew for linux"
+tar xvf krew.tar.gz ./krew-linux_amd64
+
+echo "[TASK 3] Making krew binary executable"
+chmod +x krew-linux_amd64
+
+echo "[TASK 4] Setting up krew"
+./krew-linux_amd64 install krew
+
+echo "[TASK 5] appending krew path to /home/$(logname)/.bashrc"
+echo "export PATH='${KREW_ROOT:-/home/$(logname)/.krew}/bin:$PATH'" >> /home/$(logname)/.bashrc
